c13b656a197d16a2d6da156f2ce41601e9dd4c71
jayarajr007 <132055741+jayarajr007@users.noreply.github.com>
2025-05-21 12:26:07 +0530

    update s

--- tailwind-static-starter/dist/assets/js/common.js
+++ tailwind-static-starter/dist/assets/js/common.js
@@ -27,7 +27,43 @@ $(window).on('load', function () {
             }
         ]
     });
+
+
+
+    
 });
+
+
+
+$( document ).ready(function() {
+
+    const input = document.getElementById('searchInput');
+const dropdown = document.getElementById('suggestionDropdown');
+
+input.addEventListener('focus', () => {
+    dropdown.classList.remove('hidden');
+});
+
+document.addEventListener('click', (e) => {
+    const container = document.getElementById('search-container');
+    if (!container.contains(e.target)) {
+        dropdown.classList.add('hidden');
+    }
+});
+
+    $('#openSearch').click(function () {
+      $('#searchOverlay').removeClass('hidden').addClass('block');
+    });
+
+    $('#closeSearch').click(function () {
+      $('#searchOverlay').addClass('hidden').removeClass('block');
+    });
+
+
+
+});
+
+
 
 
 
@@ -176,18 +212,6 @@ document.getElementById('category-close').addEventListener('click', function () 
 
 
 
-const input = document.getElementById('searchInput');
-const dropdown = document.getElementById('suggestionDropdown');
 
-input.addEventListener('focus', () => {
-    dropdown.classList.remove('hidden');
-});
-
-document.addEventListener('click', (e) => {
-    const container = document.getElementById('search-container');
-    if (!container.contains(e.target)) {
-        dropdown.classList.add('hidden');
-    }
-});
 
 
