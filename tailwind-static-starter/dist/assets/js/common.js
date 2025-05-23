cf268bbd5bcc3b655d022d8a1f1c364bb63487fa
jayarajr007 <132055741+jayarajr007@users.noreply.github.com>
2025-05-23 15:43:44 +0530

    css updated

--- tailwind-static-starter/dist/assets/js/common.js
+++ tailwind-static-starter/dist/assets/js/common.js
@@ -1,38 +1,6 @@
 
 
 
-$(window).on('load', function () {
-    $('.slider').slick({
-        slidesToShow: 5, // Show 5 items on large screens
-        slidesToScroll: 1,
-        dots: false,
-        arrows: true, infinite: false,
-        adaptiveHeight: false,
-        centerMode: false,  // Disable to prevent extra space
-        variableWidth: false, // Ensure uniform width
-
-
-        responsive: [
-            {
-                breakpoint: 1024, // Medium screens
-                settings: { slidesToShow: 4 }
-            },
-            {
-                breakpoint: 768, // Small screens
-                settings: { slidesToShow: 2 }
-            },
-            {
-                breakpoint: 480, // Extra small screens
-                settings: { slidesToShow: 2 }
-            }
-        ]
-    });
-
-
-
-    
-});
-
 
 
 $( document ).ready(function() {
@@ -64,20 +32,6 @@ document.addEventListener('click', (e) => {
 });
 
 
-
-
-
-
-//         $('.slider').on('setPosition', function () {
-//   let maxHeight = 0;
-//   $('.slider .slick-slide').each(function () {
-//     $(this).css('height', 'auto');
-//     if ($(this).outerHeight() > maxHeight) {
-//       maxHeight = $(this).outerHeight();
-//     }
-//   });
-//   $('.slider .slick-slide').css('height', maxHeight + 'px');
-// });
 
 
 
