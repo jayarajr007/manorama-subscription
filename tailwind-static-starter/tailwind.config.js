566936880c69c8fe44fdf02d2ecf1c88e90cea5a
jayarajr <jayarajr@mmtv.in>
2025-06-11 23:09:29 +0530

    jqueyissue and tailwind config chanbe

--- tailwind-static-starter/tailwind.config.js
+++ tailwind-static-starter/tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./dist/**/*.html'],
+  content: [
+    './*.html',            // ✅ root HTML files (very important)
+    './dist/**/*.html',    // ✅ (if you have HTML inside dist too)
+    './dist/assets/js/**/*.js'  // ✅ scan your JS files too
+  ],
   theme: {
     extend: {},
   },
