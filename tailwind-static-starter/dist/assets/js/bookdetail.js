9b80b72a05da8d9e3edeb715a17c8c87e864cec8
Hari <harisurendran94@gmail.com>
2025-06-11 22:35:55 +0530

    console error fixed

---
+++ tailwind-static-starter/dist/assets/js/bookdetail.js
@@ -0,0 +1,51 @@
+
+$(window).on('load', function () {
+    $('.related-products-slider').slick({
+        slidesToShow: 5, // Show 5 items on large screens
+        slidesToScroll: 1,
+        dots: false,
+        arrows: true, infinite: false,
+        adaptiveHeight: false,
+        centerMode: false,  // Disable to prevent extra space
+        variableWidth: false, // Ensure uniform width
+
+
+        responsive: [
+            {
+                breakpoint: 1024, // Medium screens
+                settings: { slidesToShow: 4 }
+            },
+            {
+                breakpoint: 768, // Small screens
+                settings: { slidesToShow: 3 }
+            },
+            {
+                breakpoint: 480, // Extra small screens
+                settings: { slidesToShow: 2 }
+            }
+        ]
+    });
+
+
+
+
+      $('.large-thumb-slider').slick({
+        slidesToShow: 1, // Show 5 items on large screens
+        slidesToScroll: 1,
+        dots: true,
+        arrows: true, infinite: false,
+        adaptiveHeight: false,
+        centerMode: false,  // Disable to prevent extra space
+        variableWidth: false, // Ensure uniform width
+
+
+    });
+
+
+
+
+    
+});
+
+
+
