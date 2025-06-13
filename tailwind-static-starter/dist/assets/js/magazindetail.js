ac1a63fae22f8ccdd82fb2b8bc9ffe0bf2a8ebf4
jayarajr007 <132055741+jayarajr007@users.noreply.github.com>
2025-06-13 09:53:47 +0530

    slider correction deatil page magazin

--- tailwind-static-starter/dist/assets/js/magazindetail.js
+++ tailwind-static-starter/dist/assets/js/magazindetail.js
@@ -1,49 +1,43 @@
-
-$(window).on('load', function () {
-    $('.single-issue-slider').slick({
-        slidesToShow: 5, // Show 5 items on large screens
-        slidesToScroll: 1,
-        dots: false,
-        arrows: true, infinite: false,
-        adaptiveHeight: false,
-        centerMode: false,  // Disable to prevent extra space
-        variableWidth: false, // Ensure uniform width
-
-
-        responsive: [
-            {
-                breakpoint: 1024, // Medium screens
-                settings: { slidesToShow: 4 }
-            },
-            {
-                breakpoint: 768, // Small screens
-                settings: { slidesToShow: 3 }
-            },
-            {
-                breakpoint: 480, // Extra small screens
-                settings: { slidesToShow: 2 }
-            }
-        ]
-    });
-
-
-  $('.sub-tab-btn').click(function () {
-        var tabID = $(this).data('tab');
-
-        $('.sub-tab-btn').removeClass('border-blue-500 text-blue-600 sub-active-tab');
-        $(this).addClass('border-blue-500 text-blue-600 sub-active-tab');
-
-        $('.sub-tab-content').addClass('hidden');
-        $('#' + tabID).removeClass('hidden');
-      });
-
-   
-
-
-
-
+$('.sub-tab-btn').click(function () {
+    var tabID = $(this).data('tab');
     
+    $('.sub-tab-btn').removeClass('border-blue-500 text-blue-600 sub-active-tab');
+    $(this).addClass('border-blue-500 text-blue-600 sub-active-tab');
+    
+    $('.sub-tab-content').addClass('hidden');
+    $('#' + tabID).removeClass('hidden');
+    
+    // Initialize or refresh slider only for the "single" tab
+    if (tabID === 'single') {
+        // Destroy existing slider if it exists
+        if ($('.slider-singleissue').hasClass('slick-initialized')) {
+            $('.slider-singleissue').slick('unslick');
+        }
+        
+        // Initialize new slider
+        $('.slider-singleissue').slick({
+            slidesToShow: 5,
+            slidesToScroll: 1,
+            dots: false,
+            arrows: true, 
+            infinite: false,
+            adaptiveHeight: false,
+            centerMode: false,
+            variableWidth: false,
+            responsive: [
+                {
+                    breakpoint: 1024,
+                    settings: { slidesToShow: 4 }
+                },
+                {
+                    breakpoint: 768,
+                    settings: { slidesToShow: 3 }
+                },
+                {
+                    breakpoint: 480,
+                    settings: { slidesToShow: 4 }
+                }
+            ]
+        });
+    }
 });
-
-
-
